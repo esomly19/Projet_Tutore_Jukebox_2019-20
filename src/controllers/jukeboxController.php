a8ab1019c9c21ec07c90dcff6eb5d9ccbcebed70
Thomas <esomly57@gmail.com>
2020-01-08 16:32:01 +0100

    --- Auto-Git Commit ---

---
+++ src/controllers/jukeboxController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\Controllers;
+
+use app\models\Jukebox;
+
+class jukeboxController{
+    public function __construct($container){
+        $this->container = $container;
+    }
+
+    public function showJukebox($container)
+    {
+        
+    }
+   
+
+}
