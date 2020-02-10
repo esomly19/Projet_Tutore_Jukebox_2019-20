d7b9a183d0de7ec3b8ca8dd1d3f74f5d5e59941e
Lucas <lucas.ligerot5u@etu.univ-lorraine.fr>
2020-02-10 11:33:51 +0100

    itération 2

--- application-client/public/script/main.js
+++ application-client/public/script/main.js
@@ -1,3 +1,5 @@
+const playlist = []
+let pseudo = ""
 //envoie une requête asynchrone XHR
 //params : urlSend = URL de l'api, success = fonction a appeler en cas de succés (callback)
 //return une promesse
@@ -34,17 +36,18 @@ function sendXhr(urlSend) {
 
 function showMusique(musique) {
     let content = "";
+
     musique.forEach(msq => content += `
     <div class="card mb-3">
         <div class="row no-gutters">
         <div class="col-md-2">
-            <img src="cover.jpg" class="card-img" alt="...">
+            <img src="${msq["Cover_album_big"]}" class="card-img" alt="...">
         </div>
         <div class="col-md-8">
             <div class="card-body">
-            <h5 class="card-title">${msq.titre}</h5>
+            <h5 class="card-title">${msq["Titre"]}</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <button class="btn btn-primary" text="ajouter" id="${msq.chemin}" onclick="ajouterMusique('${msq.chemin} ')">ajouter</button>
+            <button class="btn btn-primary" text="ajouter" id="${msq["Titre_album"]}" onclick="ajouterMusique('${msq["Titre_album"].replace(/\n|\r/g,'')}, ${msq["Titre"]}, ${msq["Cover_album_big"]}')">ajouter</button>
             </div>
         </div>
         </div>
@@ -53,27 +56,68 @@ function showMusique(musique) {
 
 }
 
-function ajouterMusique(musique) {
-
-        let xhr = $.ajax({
+/**
+ * permet d'envoyer une musique au jukebox
+ * en fonction du message de retour renvoyer par le serveur un message est envoyé pour tous les utilisateurs 
+ * en ligne ou en cas d'erreur un message d'erreur uniquement a la personne ayant mis le msg
+ * @param {*} musique 
+ */
+function ajouterMusique(musique, titre, image) {
+    console.log(musique)
+        $.ajax({
             url: "https://jukebox-musique.herokuapp.com/ajouterMusique",
             method: 'POST',
-            data: musique
+            data: {
+                "musique": musique,
+                "titre": titre,
+                "image": image
+            },
         })
-        console.log(xhr)
-    
-
 }
 
 function askPseudo() {
+    let content = `
+    <div>
+        <label class="h1">Entrer un pseudo</label>
+        <input type=text id="pseudo" class="input-group-text"></input>
+        <button onclick="connection()" class="btn btn-primary">valider</button>
+    </div>
+    `;
+    $("#playlist").html(content);
 
 }
 
-
-$(document).ready( () => {
-
+function connection() {
+    pseudo = document.getElementById("pseudo").value
+    document.getElementById("nav").innerHTML = "Jukebox - "+pseudo
     let urlPlaylist = "https://projetjukebox.herokuapp.com/playlist/1";
     sendXhr(urlPlaylist).then(function (data) {
-        return showMusique(data)
+        musiqueInformation(data)
     })
+}
+
+function musiqueInformation(data) {
+    data.forEach(musique =>
+        $.ajax({
+            url: "https://jukebox-admin.herokuapp.com/getMusic",
+            method: 'GET',
+            headers: {
+                "Musicname": musique.titre,
+                "chemin": musique.chemin
+            }
+            }).then(function(data) {
+                let jData = JSON.parse(data)
+                jData['Titre_album'] = musique.chemin
+                playlist.push(jData)
+            }).then(function() {
+                showMusique(playlist)
+            })
+    )        
+}
+
+$(document).ready( () => {
+    askPseudo() 
+    
+
+
 })
